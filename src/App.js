f3149c658d52781615c6c44baf00be0bf2d97f77
Xavier Bussiere <xavier.bussiere@gmail.com>
2023-08-10 00:28:59 -0400

    Changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,80 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useRef } from 'react';
+import { Scatter } from 'react-chartjs-2'
+import {
+  Chart as ChartJS,
+  LinearScale,
+  PointElement,
+  LineElement,
+} from "chart.js"
 import './App.css';
 
+ChartJS.register(LinearScale, PointElement, LineElement);
+
+const options = {
+  scales: {
+    x: {
+      type: 'linear',
+      position: 'bottom'
+    },
+    y: {
+      beginAtZero: false,
+      min: 400,
+      max: 3500
+    }
+  }
+}
+
 function App() {
+  const [co2Data, setCo2Data] = useState([]);
+  const chartRef = useRef();
+  const fetchData = async () => {
+    try {
+      const response = await fetch('http://localhost:2020/');
+      const json = await response.json();
+
+      const newPoint = {
+        x: new Date().valueOf(), // Use current time as x coordinate
+        y: json.ppm
+      };
+
+      console.log(newPoint)
+
+      setCo2Data(prevData => [...prevData, newPoint]);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+  useEffect(() => {
+    const interval = setInterval(fetchData, 5000); // Fetch data every 5 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const chartData = {
+    datasets: [{
+      label: 'Good Air Quality',
+      data: co2Data,
+      backgroundColor: value => {
+        if (value.parsed?.y <= 1000) {
+          return '#99FF00'
+        }
+        else if (value.parsed?.y <= 2000) {
+          return '#FFCC00'
+        }
+        else {
+          return '#CC0000'
+        }
+      }
+    }],
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="chart-container">
+        <h1>CO2 measurements</h1>
+        <div className="chart-wrapper">
+          <Scatter ref={chartRef} options={options} data={chartData} />
+        </div>
+      </div>
   );
 }
 
